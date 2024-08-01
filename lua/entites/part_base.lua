84a435d6db284e89a5373599afc2192548c09efa
CodeToby <wahfox8@gmail.com>
2024-07-31 20:21:00 -0400

    modified:   lua/entites/part_base.lua

--- lua/entites/part_base.lua
+++ lua/entites/part_base.lua
@@ -1,6 +1,20 @@
-ENT.Base = 'base_anim'
+AddCSLuaFile()
+DEFINE_BASECLASS( "base_anim" )
+
 ENT.ClassName = 'he_part_base'
 ENT.AdminOnly = true 
 ENT.Spawnable = false
 
-ENT.EditorName = ''
+ENT.Selected = false
+
+function ENT:Initialize()
+    if SERVER then
+        self:PhysicsInit( SOLID_VPHYSICS )
+        self:SetMoveType( MOVETYPE_VPHYSICS )
+        self:SetSolid( SOLID_VPHYSICS )
+    end
+end
+
+function ENT:Draw()
+
+end
